386ce62f9d9e0a8d21c7c6680804dcadbcf3562d
Oron Port <oron.port@dfiant.works>
2024-12-15 03:26:59 +0200

    remove redundant code

--- FromTuple.scala
+++ FromTuple.scala
@@ -4,8 +4,6 @@ import macros.*
 type ~[T] = FromTuple[T]
 opaque type FromTuple[T] <: T = T
 object FromTuple:
-  protected class Wrap[T]
-  def apply[T]: Wrap[T] = ???
   inline implicit def fromTuple[F <: Tuple, T](inline from: F): FromTuple[T] =
     ${ fromTupleMacro[F, T]('from) }
   inline implicit def fromT[T](inline from: T): FromTuple[T] = from
